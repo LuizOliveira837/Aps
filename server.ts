b1491dfaf81b65d6a1842b0ae669c8be9c5cf3f4
Doug-Oliveira <luizhorocha@gmail.com>
2021-10-19 23:21:38 -0300

    Starting

--- server.ts
+++ server.ts
@@ -1,10 +1,13 @@
 import express from 'express';
 import { routesAirQuality } from './routes/airQuality.routes';
+import cors from 'cors'
+
 
 
 const server = express();
 
 server.use(express.json())
+server.use(cors())
 
 
 server.use('/air-quality', routesAirQuality)
